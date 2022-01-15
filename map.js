24135e8d22d7483f05c57f80ddd53e0143f2e4c0
AdalbertCosta <contato@adalbertocosta.com>
2022-01-15 08:19:18 -0300

    Map Filter e reduce

---
+++ map.js
@@ -0,0 +1,22 @@
+//Apresentação do Curso
+
+//O que é, Sintaxe e Map vs forEach
+
+// Map - Quando um conjunto passa por uma operação e passa a ser  outro conjunto. (Interpretação matemática)
+
+/*
+
+Cria um novo array
+Não modifca o array original
+Realiza oprações em ordem
+
+Sintaxe**
+
+Callback: função a ser executada em cada elemento.
+
+thisArg(opcional): valor de 'this dentro da função callback.
+
+*/
+
+//Map vs forEach
+
